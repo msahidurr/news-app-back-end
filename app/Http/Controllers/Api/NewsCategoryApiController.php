950ce9eb3ed15f9759714d6bf929d9b825082b14
msahidurr <shohidurr49@gmail.com>
2024-06-02 19:50:50 +0600

    wip

--- app/Http/Controllers/Api/NewsCategoryApiController.php
+++ app/Http/Controllers/Api/NewsCategoryApiController.php
@@ -11,7 +11,13 @@ class NewsCategoryApiController extends Controller
     public function index(Request $request)
     {
         return response()->json([
-            'data'  => NewsCategory::active()->get() ?? null,
+            'data'  => NewsCategory::active()->get()->map(function($row) {
+                return [
+                    'id'    => $row->id,
+                    'title' => $row->title,
+                    'slug'  => $row->slug,
+                ];
+            }) ?? null,
         ], 200);
     }
 }
